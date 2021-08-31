86050fcd9798a1e9d348e33bf64ffe92bb5faf7d
grantfellows <gwfellows@icloud.com>
2021-08-31 13:26:52 -0700

    speed up with multiprocessing

--- draw.py
+++ draw.py
@@ -7,13 +7,13 @@ turtle.delay(0)
 
 def draw(leaf_positions, branch_positions, sx=0, sy=300, r=2):
     for x, y in leaf_positions:
-        print(x, y)
+        #print(x, y)
         turtle.goto(x, y)
         turtle.pendown()
         turtle.circle(r)
         turtle.penup()
     for x1, y1, x2, y2 in branch_positions:
-        print(x1, y1, x2, y2)
+        #print(x1, y1, x2, y2)
         turtle.goto(x1, y1)
         turtle.pendown()
         turtle.goto(x2, y2)
--- evolve.py
+++ evolve.py
@@ -1,19 +1,33 @@
 import random
 from score import fitness
 from grow import new_random_code, grow_tree, point_mutate
+from timeit import default_timer as timer
+import multiprocessing
 
-POPULATION_SIZE = 2000
-GENERATIONS = 2000
+POPULATION_SIZE = 50
+GENERATIONS = 100
 CODE_LENGTH = 200
-N_SAVE = 20
+N_SAVE = 10
 N_MUTATE = 4
+
+
+def score_code(code):
+    return fitness(*grow_tree(code))
+
 
 if __name__ == "__main__":
     population = [new_random_code(CODE_LENGTH) for _ in range(POPULATION_SIZE)]
     for n in range(GENERATIONS):
         try:
-            print(n, fitness(*grow_tree(population[0])))
-            population.sort(key=lambda code: fitness(*grow_tree(code)), reverse=True)
+            p = multiprocessing.Pool(4)
+            fitnesses = p.map(score_code, population)
+            population = [
+                x
+                for _, x in sorted(
+                    zip(fitnesses, population), key=lambda p: p[0], reverse=True
+                )
+            ]
+            print(n)
             save = population[:N_SAVE]
             for i in range(N_SAVE, POPULATION_SIZE):
                 population[i] = random.choice(save).copy()
@@ -24,8 +38,8 @@ if __name__ == "__main__":
 
     from draw import draw
 
-    print(*grow_tree(population[0]))
-    print(population[0])
+    # print(*grow_tree(population[0]))
+    # print(population[0])
     draw(*grow_tree(population[0]))
     import tkinter
 
